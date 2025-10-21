e415832a30cc4ab8e93a1d64b47eb778e58063f3
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-10-21 16:01:04 -0300

    Adicionada página inicial

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,11 +1,15 @@
 import {
-  ApplicationConfig,
-  provideBrowserGlobalErrorListeners,
-  provideZonelessChangeDetection,
+    ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection
 } from '@angular/core';
 import { provideRouter, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+  {
+    path: 'inicio',
+    loadComponent: () => import('./components/inicio/inicio').then((c) => c.Inicio),
+  },
+];
 
 export const appConfig: ApplicationConfig = {
   providers: [
--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 import { ShellComponent } from './components/shell/shell.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  imports: [ShellComponent],
+  imports: [ShellComponent, RouterOutlet],
 })
 export class App {}
