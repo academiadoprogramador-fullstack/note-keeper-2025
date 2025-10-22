8e542a6053606fdf1e8b578eec50114846ea1c8e
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-10-21 23:24:41 -0300

    Implementada estilização

--- client/src/app/components/shell/shell.component.ts
+++ client/src/app/components/shell/shell.component.ts
@@ -9,7 +9,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-shell',
@@ -23,6 +23,7 @@ import { RouterLink } from '@angular/router';
     MatIconModule,
     AsyncPipe,
     RouterLink,
+    RouterLinkActive,
   ],
 })
 export class ShellComponent {
