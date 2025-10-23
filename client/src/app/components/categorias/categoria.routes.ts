c42ffcebf007d055f94f25b144bbea30a07b3c76
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-10-23 15:29:45 -0300

    Adicionado cadastro de Categorias

--- client/src/app/components/categorias/categoria.routes.ts
+++ client/src/app/components/categorias/categoria.routes.ts
@@ -1,5 +1,9 @@
 import { Routes } from '@angular/router';
 
+import { CadastrarCategoria } from './cadastrar/cadastrar-categoria';
 import { ListarCategorias } from './listar/listar-categorias';
 
-export const categoriaRoutes: Routes = [{ path: '', component: ListarCategorias }];
+export const categoriaRoutes: Routes = [
+  { path: '', component: ListarCategorias },
+  { path: 'cadastrar', component: CadastrarCategoria },
+];
--- client/src/app/components/categorias/categoria.service.ts
+++ client/src/app/components/categorias/categoria.service.ts
@@ -4,7 +4,10 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 
 import { environment } from '../../../environments/environment';
-import { ListagemCategoriasApiResponse, ListagemCategoriasModel } from './categoria.models';
+import {
+    CadastrarCategoriaModel, CadastrarCategoriaResponseModel, ListagemCategoriasApiResponse,
+    ListagemCategoriasModel
+} from './categoria.models';
 
 @Injectable({
   providedIn: 'root',
@@ -13,6 +16,12 @@ export class CategoriaService {
   private readonly http = inject(HttpClient);
 
   private readonly apiUrl = environment.apiUrl + '/categorias';
+
+  public cadastrar(
+    categoriaModel: CadastrarCategoriaModel,
+  ): Observable<CadastrarCategoriaResponseModel> {
+    return this.http.post<CadastrarCategoriaResponseModel>(this.apiUrl, categoriaModel);
+  }
 
   public selecionarTodas(): Observable<ListagemCategoriasModel[]> {
     return this.http
