5b6f54c334ab20bb530750d474324bb489056798
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-10-23 08:53:59 -0300

    Movida shell para shared

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { ShellComponent } from './components/shell/shell.component';
+import { ShellComponent } from './components/shared/shell/shell.component';
 
 @Component({
   selector: 'app-root',
