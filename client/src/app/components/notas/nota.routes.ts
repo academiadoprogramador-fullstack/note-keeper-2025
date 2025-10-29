c2c37cdf8990d898f5449a64aebe408d04a3d3cd
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-10-29 15:02:31 -0300

    Implementada edição de Notas

--- client/src/app/components/notas/nota.routes.ts
+++ client/src/app/components/notas/nota.routes.ts
@@ -1,8 +1,9 @@
 import { inject } from '@angular/core';
-import { Routes } from '@angular/router';
+import { ActivatedRouteSnapshot, Routes } from '@angular/router';
 
 import { CategoriaService } from '../categorias/categoria.service';
 import { CadastrarNota } from './cadastrar/cadastrar-nota';
+import { EditarNota } from './editar/editar-nota';
 import { ListarNotas } from './listar/listar-notas';
 import { NotaService } from './nota.service';
 
@@ -10,6 +11,16 @@ const listagemNotasResolver = () => {
   const notaService = inject(NotaService);
 
   return notaService.selecionarTodas();
+};
+
+const detalhesNotaResolver = (route: ActivatedRouteSnapshot) => {
+  const notaService = inject(NotaService);
+
+  if (!route.paramMap.has('id')) throw new Error('O parâmetro id não foi fornecido.');
+
+  const notaId = route.paramMap.get('id')!;
+
+  return notaService.selecionarPorId(notaId);
 };
 
 const listagemCategoriasResolver = () => {
@@ -27,6 +38,11 @@ export const notaRoutes: Routes = [
         path: 'cadastrar',
         component: CadastrarNota,
         resolve: { categorias: listagemCategoriasResolver },
+      },
+      {
+        path: 'editar/:id',
+        component: EditarNota,
+        resolve: { nota: detalhesNotaResolver, categorias: listagemCategoriasResolver },
       },
     ],
     providers: [NotaService, CategoriaService],
